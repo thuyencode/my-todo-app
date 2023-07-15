b4fe98f66f47c269626ff6c3a266e4526ebf65b0
thuyencode <thuyencode@proton.me>
2023-07-15 20:25:31 +0700

    🚚 Move from TailwindCSS to UnoCSS

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react-swc'
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    UnoCSS(),
     react(),
     legacy({
       targets: ['defaults', 'not IE 11']
